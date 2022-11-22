fb33a8d62c4e6d1370028c0b63fdd6d0fc03b587
Nika <99128017+Tesla0802@users.noreply.github.com>
2022-11-22 14:13:22 +0400

    navbar and bg

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDbudKZiIoJ0uA9qzj8jnH3aqrsKyqpqGU",
+    authDomain: "literaturuli-klubi.firebaseapp.com",
+    databaseURL: "https://literaturuli-klubi-default-rtdb.firebaseio.com",
+    projectId: "literaturuli-klubi",
+    storageBucket: "literaturuli-klubi.appspot.com",
+    messagingSenderId: "743192993134",
+    appId: "1:743192993134:web:72fccaf7a15c07483c357f",
+    measurementId: "G-073EXFFK1J"
+  };
+  firebase.initializeApp(firebaseConfig);
