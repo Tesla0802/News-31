b422bce9ae3dcf8a7c91a51ac130bbfba4ff266d
Nika <99128017+Tesla0802@users.noreply.github.com>
2022-11-28 21:00:18 +0400

    update

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const cards =  document.querySelector(".cards")
+const Variability = document.querySelector(".form-switch");
+const lable = document.querySelector(".form-check-label")
+const body = document.querySelector("body");
+
 function displayAlert(title, text, icon) {
     Swal.fire({
       icon: icon,
@@ -6,3 +10,32 @@ function displayAlert(title, text, icon) {
       title: title,
     });
   }
+
+  // let logic = true
+
+  // Variability.addEventListener("click", () => {
+  //   if(logic){
+  //     body.style.backgroundImage = "url(./img/dark\ bg.png)";
+  //   }else{
+  //     body.style.backgroundImage = "url(./img/light\ bg.png)";
+  //   }
+
+  //   logic = !logic
+  // })
+
+
+//   let logic = true;
+
+// Variability.addEventListener("click", () => {
+//   if (logic) {
+//     body.style.backgroundImage = "url(./img/dark\ bg.png)";
+//   } else {
+//     body.style.backgroundImage = "url(./img/light\ bg.png)";
+//   }
+//   logic = !logic;
+// });
+
+
+Variability.addEventListener("click", () =>{
+    body.style.backgroundColor = "#F7B0BB"
+})
